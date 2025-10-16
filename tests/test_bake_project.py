d54cbce23a6cef2d6c9eb03b0da82508562d83bc
jcook3701 <jcook3701@gmail.com>
2025-10-16 04:15:40 -0700

    need to test but this should mostly work.

---
+++ tests/test_bake_project.py
@@ -0,0 +1,31 @@
+# coding: utf-8
+"""Sphinx Cookiecutter Project
+
+© All rights reserved. Jared Cook
+
+See the LICENSE.TXT file for more details.
+
+Author: Jared Cook
+Description: 
+"""
+
+def test_bake_with_defaults(cookies):
+    """Ensure the template bakes correctly with default context."""
+    result = cookies.bake()
+    assert result.exit_code == 0
+    assert result.exception is None
+    assert result.project_path.is_dir()
+
+    # Optional sanity checks
+    project_name = result.project_path.name
+    assert project_name  # non-empty
+    readme_file = result.project_path / "README.md"
+    assert readme_file.exists()
+
+def test_bake_with_custom_name(cookies):
+    """Ensure custom project_name works."""
+    result = cookies.bake(extra_context={"project_name": "test_project"})
+    assert result.exit_code == 0
+    assert result.exception is None
+    assert result.project_path.is_dir()
+    assert result.project_path.name == "test_project"
---
+++ {{ cookiecutter.project_name }}/index.rst
@@ -0,0 +1,12 @@
+{{ cookiecutter.project_name }} documentation
+====================================
+
+.. toctree::
+   :maxdepth: 2
+   :caption: Contents:
+
+.. automodule:: {{ cookiecutter.package_name }}
+   :members:
+   :undoc-members:
+   :show-inheritance:
+

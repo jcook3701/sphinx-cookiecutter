ca499274f3655a41f8c0bd8060a17e39350721a7
jcook3701 <jcook3701+github@gmail.com>
2025-12-28 15:20:53 -0800

    Update template

--- tests/test_bake_project.py
+++ tests/test_bake_project.py
@@ -1,15 +1,27 @@
-# coding: utf-8
-"""Sphinx Cookiecutter Project
+# test_bake_project.py for sphinx-cookiecutter
+#
+# Copyright (c) 2025, Jared Cook
+# SPDX-License-Identifier: GPL-3.0-or-later
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <www.gnu.org>.
+#
 
-© All rights reserved. Jared Cook
 
-See the LICENSE.TXT file for more details.
+from pytest_cookies.plugin import Cookies
 
-Author: Jared Cook
-Description: 
-"""
 
-def test_bake_with_defaults(cookies):
+def test_bake_with_defaults(cookies: Cookies) -> None:
     """Ensure the template bakes correctly with default context."""
     result = cookies.bake()
     assert result.exit_code == 0
@@ -19,13 +31,15 @@ def test_bake_with_defaults(cookies):
     # Optional sanity checks
     project_name = result.project_path.name
     assert project_name  # non-empty
-    readme_file = result.project_path / "README.md"
-    assert readme_file.exists()
+    test_file = result.project_path / "Makefile"
+    assert test_file.exists()
 
-def test_bake_with_custom_name(cookies):
+
+def test_bake_with_custom_name(cookies: Cookies) -> None:
     """Ensure custom project_name works."""
-    result = cookies.bake(extra_context={"project_name": "test_project"})
+    result = cookies.bake(extra_context={"project_name": "Test Project"})
+    print(result)
     assert result.exit_code == 0
     assert result.exception is None
     assert result.project_path.is_dir()
-    assert result.project_path.name == "test_project"
+    assert result.project_path.name == "test-project"
